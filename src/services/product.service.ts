ba755df400326b725559256126ffd6147c8e1908
michael-p37 <michaelp_99@outlook.com>
2026-04-22 18:42:12 -0700

    fix: returns product data from product.service to match the product type

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -4,23 +4,23 @@ import { Product } from "@/models/product.model";
 export async function getProductsByCategoryId(
   categoryId: number
 ): Promise<Product[] | null> {
-  const res = await fetch(`${BASE_URL}/${categoryId}/products`);
+  const res = await fetch(`${BASE_URL}/categories/${categoryId}/products`);
   if (!res.ok) {
     const errorMessage = await res.text();
     console.log(`Error getting products by category Id ${errorMessage}`);
     return null;
   }
-  const productsResponse: Product[] = await res.json();
-  return productsResponse;
+  const productsResponse = await res.json();
+  return productsResponse.data;
 }
 
 export async function getProductById(id: number): Promise<Product | null> {
-  const res = await fetch(`${BASE_URL}/${id}`);
+  const res = await fetch(`${BASE_URL}/products/${id}`);
   if (!res.ok) {
     const errorMessage = await res.text();
     console.log(`Error getting product by Id ${errorMessage}`);
     return null
   }
   const productResponse = await res.json();
-  return productResponse;
+  return productResponse.data;
 }
